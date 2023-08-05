c10339f81717150e4fa14237e1a6bc4ab3e9c0ec
Vladimir Shardinov <vvshard@gmail.com>
2023-08-05 12:55:23 +0300

    start mod player

--- src/main.rs
+++ src/main.rs
@@ -20,12 +20,50 @@ fn main() -> Result<(), eframe::Error> {
             ui.add(egui::Slider::new(&mut 50, 0..=120).text("/120"));
             ui.horizontal(|ui| {
                 button(ui, "Play/Pause", || {});
-                button(ui, "Stop", || {println!("Stop")});
+                button(ui, "Stop", || println!("Stop"));
                 button(ui, "Prev", || {});
                 button(ui, "Next", || {});
             })
         });
     })
-    
 }
 
+mod player {
+
+    pub struct Track {
+        pub title: String,
+        pub duration: u32,
+        cursor: u32,
+    }
+
+    impl Track {
+        pub fn new(title: &'static str, duration: u32) -> Self {
+            Self { title: title.into(), duration, cursor: 0 }
+        }
+    }
+
+    /// A music player holds a playlist and it can do basic operations over it.
+    pub struct Player {
+        state: State,
+        playlist: Vec<Track>,
+        current_track: usize,
+    }
+
+    impl Default for Player {
+        fn default() -> Self {
+            let playlist = vec![
+                Track::new("Track 1", 180),
+                Track::new("Track 2", 165),
+                Track::new("Track 3", 197),
+                Track::new("Track 4", 205),
+            ];
+            Self { state: State::Stopped, playlist, current_track: 0 }
+        }
+    }
+
+    enum State {
+        Stopped,
+        Paused,
+        Playing,
+    }
+}
